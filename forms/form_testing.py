0b87d374f0c5ee02d63d45eede27d8031a188b51
Chris Castellano <c.castellano18@gmail.com>
2020-12-17 12:35:03 -0600

    shadowbox closes

--- forms/form_testing.py
+++ forms/form_testing.py
@@ -7,12 +7,15 @@ chrome_browser.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html
 
 assert "Selenium Easy Demo" in chrome_browser.title
 text_button = chrome_browser.find_element_by_class_name('btn-default')
-print(text_button.get_attribute('innerHTML'))
+# print(text_button.get_attribute('innerHTML'))
 assert 'Show Message' in chrome_browser.page_source
  
 user_message =  chrome_browser.find_element_by_id('user-message')
 user_message.clear()
 user_message.send_keys("Hello World")
 
+lightbox_close_x = chrome_browser.find_element_by_id("at-cv-lightbox-close")
+
+lightbox_close_x.click()
 
 
