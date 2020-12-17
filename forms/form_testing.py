7bee5f87694711fbacbf22cebc9e47f84b6f12f6
Chris Castellano <chriscastellano@Chriss-Air.attlocal.net>
2020-12-17 11:57:12 -0600

    message testing

--- forms/form_testing.py
+++ forms/form_testing.py
@@ -6,5 +6,13 @@ chrome_browser.maximize_window()
 chrome_browser.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
 
 assert "Selenium Easy Demo" in chrome_browser.title
-button = chrome_browser.find_element_by_class_name('btn-default')
-print(button.get_attribute('innerHTML'))
+text_button = chrome_browser.find_element_by_class_name('btn-default')
+print(text_button.get_attribute('innerHTML'))
+assert 'Show Message' in chrome_browser.page_source
+ 
+user_message =  chrome_browser.find_element_by_id('user-message')
+user_message.clear()
+user_message.send_keys("Hello World")
+
+text_button.click()
+
